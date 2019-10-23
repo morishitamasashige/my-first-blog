9d3ac07f4f8dd30bcae5b18ecf6ce3030aeb3a3a
morishitamasashige <masashige.morishita@gmail.com>
2019-10-23 11:09:04 +0900

    delete sevelal lines

--- IdeaWorthSharing/models.py
+++ IdeaWorthSharing/models.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from django.db import models
 from django.utils import timezone
-from taggit.managers import TaggableManager
+#from taggit.managers import TaggableManager
 
 # Create your models here.
 
@@ -9,7 +9,7 @@ class IdeaTweet(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, blank=True)
     context = models.CharField(max_length=200)
     published_date = models.DateTimeField(blank=True, null=True)
-    tags = TaggableManager(blank=True)
+    #tags = TaggableManager(blank=True)
 
     def publish(self):
         self.published_date = timezone.now()
